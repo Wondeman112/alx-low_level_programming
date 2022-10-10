865e82d784ba303896ce85b419afb11595896d42
Wondeman112 <wondem507@gmail.com>
2022-10-10 14:11:50 -0700

    my commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,16 +13,12 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
 
-	if (d == 0)
-
-		return;
-
-
-
-	d->name = name;
-
-	d->age = age;
-
-	d->owner = owner;
+	if (d != Null)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	
 
 }
